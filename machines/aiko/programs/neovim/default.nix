6dd887e2471289b75d39a9430dfec339f339399f
inalone <me@inal.one>
2024-03-22 23:18:14 +0000

    Add treesitter config

--- machines/aiko/programs/neovim/default.nix
+++ machines/aiko/programs/neovim/default.nix
@@ -21,7 +21,6 @@ in
     vimAlias = true;
 
     plugins = [
-      treesitterWithGrammars
     ];
   };
 
@@ -30,12 +29,8 @@ in
     recursive = true;
   };
 
-  # Treesitter is configured as a locally developed module in lazy.nvim
-  # we hardcode a symlink here so that we can refer to it in our lazy config
-  home.file."./.local/share/nvim/nix/nvim-treesitter/" = {
-    recursive = true;
-    source = treesitterWithGrammars;
-  };
-
+  xdg.configFile."nvim/parser".source = "${pkgs.symlinkJoin {
+      name = "treesitter-parsers";
+      paths = treesitterWithGrammars.dependencies;
+    }}/parser";
 }
-
---
+++ machines/aiko/programs/neovim/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+	end,
+}
